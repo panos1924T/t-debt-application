5df120875faaf36f11b45a39240e476331b9d90c
PanTs <pants.ath@gmail.com>
2026-07-01 16:22:27 +0300

    feat: initialize project

---
+++ src/main/java/gr/pants/tdebt/TdebtApplication.java
@@ -0,0 +1,13 @@
+package gr.pants.tdebt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdebtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdebtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/pants/tdebt/TdebtApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.pants.tdebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdebtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
